b262563f0a035fa5830a29bc03e4a9ed0b073572
alexpanmk <alexpanmk@gmail.com>
2024-01-13 14:43:32 +0800

    refactor Frontpage component

--- SEIF-PROJ3-FE/src/Frontpage/Frontpage.jsx
+++ SEIF-PROJ3-FE/src/Frontpage/Frontpage.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import DisplayCard from "../DisplayCard/DisplayCard";
 
+//TODO: To fetch cards to render for DisplayCards, to create useElysioAPI for logic to create the display sequence.
+
 function Frontpage() {
+  const [monthArray, setMonthArray] = useState([]); //For the days
+  const [quoteArray, setQuoteArray] = useState([]); // For the quotes
+
+  useEffect(() => {
+    setMonthArray(generateMonthArray(2021, 8));
+  }, []);
+
   return (
     <>
-      <div class="ml-6 mr-6 gap-8 columns-3 ">
+      <div class="ml-6 mr-6 gap-8 columns-4 ">
         <DisplayCard />
         <img
           class="w-full aspect-video mb-6"
@@ -48,6 +57,23 @@ function Frontpage() {
       </div>
     </>
   );
+}
+
+/////////////// Utility Functions ///////////////
+
+function generateMonthArray(year, month) {
+  let monthArray = [];
+  let date = new Date(year, month, 1);
+
+  while (date.getMonth() === month) {
+    monthArray.push({
+      dateNumber: date.getDate(),
+      day: date.toLocaleString("en-US", { weekday: "long" }),
+    });
+    date.setDate(date.getDate() + 1);
+  }
+
+  return monthArray;
 }
 
 export default Frontpage;
