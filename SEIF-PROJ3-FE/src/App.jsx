4032ccae6831f436aa4f150d2e64c4e02030cc5e
alexpanmk <alexpanmk@gmail.com>
2024-01-13 11:15:06 +0800

    feat: :sparkles: add Navbar component

--- SEIF-PROJ3-FE/src/App.jsx
+++ SEIF-PROJ3-FE/src/App.jsx
@@ -2,12 +2,14 @@ import { React, useState, useEffect } from "react";
 
 //Elysio Components
 import Frontpage from "./Frontpage/Frontpage";
+import Navbar from "./Navbar/Navbar";
 
 //TODO: Daisy UI 30 x Card Array for Frontpage
 
 export default function App() {
   return (
     <>
+      <Navbar />
       <Frontpage />
     </>
   );
