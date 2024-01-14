4c857f5bad392ca6a1411e2ffefc839f91da771c
alexpanmk <alexpanmk@gmail.com>
2024-01-15 00:08:55 +0800

    refactor: update Frontpage

--- SEIF-PROJ3-FE/src/DayCard/DayCard.jsx
+++ SEIF-PROJ3-FE/src/DayCard/DayCard.jsx
@@ -1,22 +1,26 @@
-import React from "react";
+import { React, useState } from "react";
 
-function DayCard() {
+function DayCard({ dateNo, day }) {
+  console.log(dateNo);
   return (
-    <div className="h-96 my-8 card bordered shadow-lg bg-primary">
-      <div className="card-body">
-        {/* <h2 className="card-title">Card Title</h2> */}
-        {/* <p>
+    <>
+      <div className="h-96 my-8 card bordered shadow-lg bg-primary">
+        <div className="card-body">
+          {/* <h2 className="card-title">Card Title</h2> */}
+          {/* <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
           necessitatibus incidunt ut officiis explicabo inventore.
         </p> */}
 
-        <p className="text-9xl font-bold">28</p>
+          <h1 className="text-9xl font-bold">{dateNo}</h1>
+          <p className="text-2xl font-bold">{day}</p>
 
-        {/* <div className="justify-end card-actions">
+          {/* <div className="justify-end card-actions">
           <button className="btn btn-secondary">More Info</button>
         </div> */}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
