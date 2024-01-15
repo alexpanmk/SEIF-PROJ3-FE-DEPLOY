1af361fb4e7e9989cb3eab4c6be119bedeb58537
alexpanmk <alexpanmk@gmail.com>
2024-01-15 09:57:46 +0800

    refactor: update app.jsx

--- SEIF-PROJ3-FE/src/App.jsx
+++ SEIF-PROJ3-FE/src/App.jsx
@@ -24,7 +24,7 @@ export default function App() {
       {" "}
       {/* <-- Wrap your application with Router */}
       <main className="App">
-        {!user ? (
+        {user ? (
           <>
             <Navbar />
             <Routes>
