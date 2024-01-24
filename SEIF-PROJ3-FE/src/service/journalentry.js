59e142d68d781419e7436657ed3f123777f28d46
alexpanmk <alexpanmk@gmail.com>
2024-01-25 00:28:34 +0800

    fix: to include JWT in request and inc user(email) in payload

--- SEIF-PROJ3-FE/src/service/journalentry.js
+++ SEIF-PROJ3-FE/src/service/journalentry.js
@@ -1,6 +1,12 @@
 import * as journalAPI from '../api/journalentry';
 
 export async function createJournalEntry(entryData) {
+
+  //Append user in entryData
+  const token = getToken();
+  const user = JSON.parse(atob(token.split(".")[1])).payload.email;
+  entryData.user = user;
+
   // Delegate the network request code to the journalentry-api.js API module
   return await journalAPI.createJournalEntry(entryData);
 }
